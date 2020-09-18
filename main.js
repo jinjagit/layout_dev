e5e1f5c024b369947ab59f5ca9b7c2b344021408
Simon Tharby <jinjamail@gmail.com>
2020-09-18 21:17:20 +0100

    display screen & window dimensions

---
+++ main.js
@@ -0,0 +1,12 @@
+let x = screen.width;
+let y = screen.height;
+let widthText = document.getElementById("width");
+let heightText = document.getElementById("height");
+widthText.innerHTML = `width: ${x}`;
+heightText.innerHTML = `height: ${y}`;
+x = window.innerWidth;
+y = window.innerHeight;
+let win_widthText = document.getElementById("win-width");
+let win_heightText = document.getElementById("win-height");
+win_widthText.innerHTML = `width: ${x}`;
+win_heightText.innerHTML = `height: ${y}`;
